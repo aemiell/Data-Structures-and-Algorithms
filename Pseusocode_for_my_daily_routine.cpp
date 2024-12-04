b424978c509ec8e9f9a9d44fb4252b2410626d0e
aemiell <aemiellabasan7@gmail.com>
2024-12-04 10:55:58 +0800

    Create_Pseusocode_for_my_daily_routine.cpp

---
+++ Pseusocode_for_my_daily_routine.cpp
@@ -0,0 +1,22 @@
+Start
+↓
+#This is a process of how I clean my dogs house
+#I'll check first if dog food is still available
+If dog food is available
+Proceed
+    ↓
+Else buy dog food and go back to the first step; (We can also use loop statement so the process can repeat)
+    ↓
+Clean the cage or house of my dogs
+    ↓
+Check the water in the bowl
+If bowl is empty: 
+Refill water on the bowl
+    ↓
+Else if the water on the bowl is dirty
+Refill water on the bowl
+    ↓
+Else:
+Water is clean so no need to refill
+↓
+End
